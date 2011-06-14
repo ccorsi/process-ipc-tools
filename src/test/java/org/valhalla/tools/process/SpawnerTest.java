05cffbb1c84bc6fed6e23fda9dcc9cdb112ad9dd
Claudio Corsi <claudio@fusesource.com>
2011-06-13 22:10:29 -0400

    Added license agreement.
    Updated the testcase to use JUnit 4 instead of JUnit 3.

--- src/test/java/org/valhalla/tools/process/SpawnerTest.java
+++ src/test/java/org/valhalla/tools/process/SpawnerTest.java
@@ -1,45 +1,43 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.valhalla.tools.process;
+
+import static org.junit.Assert.*;
 
 import java.io.File;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
 /**
  * Unit test for simple Spawner.
  */
 public class SpawnerTest 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public SpawnerTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( SpawnerTest.class );
-    }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
+    @Test public void testApp()
     {
     	System.out.println(new File("."));
         assertTrue( true );
     }
     
-    public void testSpawnedClassLoader() throws Exception {
+    @Test public void testSpawnedClassLoader() throws Exception {
     	System.setProperty("spawned.property", "I'M SET");
     	System.out.println("INSIDE testSpawnedClassLoader");
     	Spawner spawner = new Spawner(RunMeExecute.class.getName());
